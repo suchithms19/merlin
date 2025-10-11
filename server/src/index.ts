c38556e1925ac3c589c254ec700ac590e2a6a66f
suchithms19 <mssuchith2@gmail.com>
2025-10-11 13:35:22 +0530

    feat: Add UUID dependency and implement deployment endpoint

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,5 +1,7 @@
 import express from 'express';
 import cors from 'cors';
+import { v4 as uuidv4 } from 'uuid';
+
 
 const app = express();
 const PORT = 3000;
@@ -9,6 +11,17 @@ app.use(cors())
 
 app.get('/ping', (_req, res) => {
   res.send('Pong');
+});
+
+app.post('/deploy', (req, res) => {
+  const { repoUrl } = req.body;
+
+  if (!repoUrl) {
+    return res.status(400).send('Repository URL is required');
+  }
+
+  console.log(`Deploying repository from ${repoUrl}`);
+  res.send('Deployment started');
 });
 
 app.listen(PORT, () => {
