9f5bca409582c3618a04d3eb46191cff248e8a7d
suchithms19 <mssuchith2@gmail.com>
2025-10-19 18:59:46 +0530

    feat: add async route handler for all GET requests

--- request-handler/src/index.ts
+++ request-handler/src/index.ts
@@ -7,6 +7,10 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.get('/*', async (req, res) => { 
+    
+});
+
 app.listen(4000, () => {
     console.log('Request handler is running on port 4000');
 });
